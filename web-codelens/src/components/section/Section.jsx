386251b0ef2d508b915a15d41b5511701b761d3d
Yanina-Unrein <unrein.syanina@gmail.com>
2024-12-28 21:23:11 -0300

    componente section, agregado de imagenes y se sacaron estilos por defecto

---
+++ web-codelens/src/components/section/Section.jsx
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types';
+import './section.css';
+
+const Section = ({ children, className = '' }) => {
+  const sectionClass = `section ${className}`.trim();
+
+  return <section className={sectionClass}>{children}</section>;
+};
+
+Section.propTypes = {
+  children: PropTypes.node.isRequired,
+  className: PropTypes.string,
+};
+
+export default Section;

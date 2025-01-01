1641ee8a02477c1a4f79028a45de8996ee301274
Loreaylen <lag9512@gmail.com>
2025-01-01 17:26:00 -0300

    Footer: Se agregaron urls
    Falta la url de facebook. Se agregó una ruta para preguntas frecuentes y contacto. Tener en cuenta que los paths están incompletos

--- web-codelens/src/components/footer/Footer.jsx
+++ web-codelens/src/components/footer/Footer.jsx
@@ -5,38 +5,38 @@ export default function Footer() {
     <footer>
       <div className='firstRowContainer'>
         <div className="socialLogoContainer">
-          <a href="#"><img id="footerLogo" src="/src/assets/icons/logo_Completo.svg" alt="Logo" /></a>
+          <a href="/"><img id="footerLogo" src="/src/assets/icons/logo_Completo.svg" alt="Logo" /></a>
           <span>Convierte ideas en excelencia en línea con nuestras soluciones innovadoras y servicios digitales estratégicos</span>
           <div className="mediaContainer">
-            <a href="#"><img src="/src/assets/icons/icon_instagram.svg" alt="Instagram" /></a>
+            <a href="https://www.instagram.com/codelens.tech/"><img src="/src/assets/icons/icon_instagram.svg" alt="Instagram" /></a>
             <a href="#"><img src="/src/assets/icons/icon_facebook.svg" alt="Facebook" /></a>
           </div>
         </div>
         <div className="servicesContainer">
           <span className="containerTitle">Servicios</span>
           <ul className="footerList">
-            <li><a href="#">Desarrollo Web</a></li>
-            <li><a href="#">Diseño Web</a></li>
-            <li><a href="#">Mejora y Optimización</a></li>
-            <li><a href="#">Desarrollo de Aplicaciones Web</a></li>
-            <li><a href="#">Desarrollo de APIS</a></li>
+            <li><a href="/servicios/desarrollo-web">Desarrollo Web</a></li>
+            <li><a href="/servicios/diseno-web">Diseño Web</a></li>
+            <li><a href="/servicios/seo">Mejora y Optimización</a></li>
+            <li><a href="/servicios/app-web">Desarrollo de Aplicaciones Web</a></li>
+            <li><a href="/servicios/desarrollo-api">Desarrollo de APIS</a></li>
           </ul>
         </div>
         <div className="wayContainer">
           <span className="containerTitle">Encuentra tu camino</span>
           <ul className="footerList">
-            <li><a href="#">Inicio</a></li>
-            <li><a href="#">Sobre Nosotros</a></li>
-            <li><a href="#">Blog</a></li>
+            <li><a href="/">Inicio</a></li>
+            <li><a href="/nosotros">Sobre Nosotros</a></li>
+            <li><a href="/blog">Blog</a></li>
           </ul>
         </div>
         <div className="helpContainer">
           <span className="containerTitle">Ayuda</span>
           <ul className="footerList">
-            <li><a href="#">FAQs</a></li>
-            <li><a href="#">Contacto</a></li>
-            <li><a href="#">Términos y condiciones</a></li>
-            <li><a href="#">Política de privacidad</a></li>
+            <li><a href="/preguntas-frecuentes">FAQs</a></li>
+            <li><a href="/contacto">Contacto</a></li>
+            <li><a href="/terminos-y-condiciones">Términos y condiciones</a></li>
+            <li><a href="/politica-de-privacidad">Política de privacidad</a></li>
           </ul>
         </div>
       </div>

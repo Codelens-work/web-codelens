3c61654d9e84c6b3accf3198c45953dcad1015f6
Loreaylen <lag9512@gmail.com>
2025-02-05 22:38:14 -0300

    Footer: Modifiqué lógica de footer

--- web-codelens/src/components/footer/Footer.jsx
+++ web-codelens/src/components/footer/Footer.jsx
@@ -1,49 +1,49 @@
 import './footer.css'
 import { useTranslation } from 'react-i18next'
 
-export function Footer() {
+export default function Footer() {
 
   const { t } = useTranslation()
 
-  const lists = t('footer-section.lists', { returnObjects: true } )
+  const footer = t('footer-section', { returnObjects: true } )
   
+  const {"services-list": servicesList, "find-way-list": findWayList, "help-list": helpList} = footer.lists;
+  
+  console.log(servicesList)
 
   return (
     <footer>
       <div className='firstRowContainer'>
         <div className="socialLogoContainer">
           <a href="/"><img id="footerLogo" src="/icons/logo_completo.svg" alt="Logo" /></a>
-          <span>Convierte ideas en excelencia en línea con nuestras soluciones innovadoras y servicios digitales estratégicos</span>
+          <span>{footer.intro}</span>
           <div className="mediaContainer">
             <a href="https://www.instagram.com/codelens.tech/" target='_blank'><img src="/icons/icon_instagram.svg" alt="Instagram" /></a>
             <a href="#" target='_blank'><img src="/icons/icon_facebook.svg" alt="Facebook" /></a>
           </div>
         </div>
         <div className="servicesContainer">
-          <span className="containerTitle">Servicios</span>
+          <span className="containerTitle">{servicesList.title}</span>
           <ul className="footerList">
-            <li><a href="/servicios/desarrollo-web">Desarrollo Web</a></li>
-            <li><a href="/servicios/diseno-web">Diseño Web</a></li>
-            <li><a href="/servicios/seo">Mejora y Optimización</a></li>
-            <li><a href="/servicios/app-web">Desarrollo de Aplicaciones Web</a></li>
-            <li><a href="/servicios/desarrollo-api">Desarrollo de APIS</a></li>
+            {servicesList.items.map((item, i) => {
+              return <li key={i}><a href={item.url}>{item.text}</a></li>
+            })}
           </ul>
         </div>
         <div className="wayContainer">
-          <span className="containerTitle">Encuentra tu camino</span>
+          <span className="containerTitle">{findWayList.title}</span>
           <ul className="footerList">
-            <li><a href="/">Inicio</a></li>
-            <li><a href="/nosotros">Sobre Nosotros</a></li>
-            <li><a href="/blog">Blog</a></li>
+          {findWayList.items.map((item, i) => {
+              return <li key={i}><a href={item.url}>{item.text}</a></li>
+            })}
           </ul>
         </div>
         <div className="helpContainer">
-          <span className="containerTitle">Ayuda</span>
+          <span className="containerTitle">{helpList.title}</span>
           <ul className="footerList">
-            <li><a href="/preguntas-frecuentes">FAQs</a></li>
-            <li><a href="/contacto">Contacto</a></li>
-            <li><a href="/terminos-y-condiciones">Términos y condiciones</a></li>
-            <li><a href="/politica-de-privacidad">Política de privacidad</a></li>
+          {helpList.items.map((item, i) => {
+              return <li key={i}><a href={item.url}>{item.text}</a></li>
+            })}
           </ul>
         </div>
       </div>

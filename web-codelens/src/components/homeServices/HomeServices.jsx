20f3e4c642a2db24b9d033dffc3c049b8914ceef
Loreaylen <lag9512@gmail.com>
2025-02-15 17:21:13 -0300

    Home services: map de cards

--- web-codelens/src/components/homeServices/HomeServices.jsx
+++ web-codelens/src/components/homeServices/HomeServices.jsx
@@ -1,14 +1,26 @@
 import Section from '../section/Section';
+import cardHomeService from '../cardHomeService/cardHomeService';
 import './homeServices.css'
 
 const HomeServices = ({ t }) => {
-
+const services = t.cards
 return(
   <Section className='home-services-section'>
     <h2 className='home-services-h2'>{t.heading}</h2>
     <span>{t.intro}</span>
     <div className='home-services-cards'>
-
+    {services.map((service, i) => {
+      <cardHomeService
+      key={service.i+ service.url}
+      title={service.title}
+      content={service.content}
+      icon={service.icon}
+      serviceUrl={service.url}
+      cta={t['services-card-cta']}
+      />
+    }
+    
+    )}
     </div>
   </Section>
 )

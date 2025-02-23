801c1713aa8e6ac54135df5c6564788c811a9760
Loreaylen <lag9512@gmail.com>
2025-02-23 12:42:43 -0300

    Home carousel: Agrego funciones para evento touch
    Falta lógica

--- web-codelens/src/components/homeServices/HomeServices.jsx
+++ web-codelens/src/components/homeServices/HomeServices.jsx
@@ -1,11 +1,26 @@
 import Section from '../section/Section';
 import CardHomeService from '../cardHomeService/cardHomeService';
 import './homeServices.css'
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 
 const HomeServices = ({ t }) => {
   const services = t.cards
   const [activeSlide, setActiveSlide] = useState(0)
+  const [isTouching, setIsTouching] = useState(false)
+  const [prevItemX, setPrevItemX] = useState(undefined)
+  const [prevScrollLeft, setPrevScrollLeft] = useState(undefined)
+  const carouselRef = useRef(null)
+
+  const scrollStart = (e) => {
+    setIsTouching(true);
+    setPrevItemX(e.touches[0].pageX)
+    setPrevScrollLeft(carouselRef.current.scrollLeft) 
+  }
+
+  const scrolling = (e) => {
+    if(!isTouching) return;
+    carouselRef.current.scrollLeft = e.touches[0].pageX
+  }
 
   return (
     <Section className='home-services-section'>
@@ -14,7 +29,13 @@ const HomeServices = ({ t }) => {
         <span>{t.intro}</span>
       </div>
       <div className='home-services-carousel'>
-        <div className='home-carousel-slides'>
+        <div 
+          className='home-carousel-slides' 
+          ref={carouselRef} 
+          onTouchStart={(e) => scrollStart(e)} 
+          onTouchMove={(e) => scrolling(e)}
+          onTouchEnd={() => setIsTouching(false)}
+        >
           {services.map((service, i) => {
             return <CardHomeService
               activeClass={activeSlide === i ? "activeSlide" : ""}

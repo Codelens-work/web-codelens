d485679aba6095dfd65624cb7edcf44469e038f8
Nico <nicolasromero.barrios@gmail.com>
2025-02-27 17:59:48 -0300

    add medium large card

--- web-codelens/src/pages/blog/Blog.jsx
+++ web-codelens/src/pages/blog/Blog.jsx
@@ -1,31 +1,35 @@
-import './blog.css';
-import { Hero } from '../../components/hero/Hero';
-import { useTranslation } from 'react-i18next'
+import "./blog.css";
+import { Hero } from "../../components/hero/Hero";
+import { useTranslation } from "react-i18next";
+import BlogCard from "../../components/blogCard/BlogCard";
 
 const Blog = () => {
-
-  const { t } = useTranslation()
+  const { t } = useTranslation();
 
   return (
-    <Hero 
-      media={{
-        type: 'image',
-        src: '/hero/hero-blog.webp',
-      }}
-    >
-      <div className="blog-hero-content">
-        <h1 className="blog-title">
-          {t('blog.hero-section.heading')}
-        </h1>
-        <h2 className="blog-subtitle">
-          {t('blog.hero-section.sub-heading')}
+    <>
+      <Hero
+        media={{
+          type: "image",
+          src: "/hero/hero-blog.webp",
+        }}
+      >
+        <div className="blog-hero-content">
+          <h1 className="blog-title">{t("blog.hero-section.heading")}</h1>
+          <h2 className="blog-subtitle">
+            {t("blog.hero-section.sub-heading")}
           </h2>
-        <p className="blog-description">
-          {t('blog.hero-section.description')}
-        </p>
+          <p className="blog-description">
+            {t("blog.hero-section.description")}
+          </p>
+        </div>
+      </Hero>
+      <div className="card-container-blog">
+
+      <BlogCard />
       </div>
-    </Hero>
+    </>
   );
-}
+};
 
 export default Blog;

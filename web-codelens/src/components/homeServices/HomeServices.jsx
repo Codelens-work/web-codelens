04dbbc213034b0a86ef3a4ece7089d754803b00b
Loreaylen <lag9512@gmail.com>
2025-02-28 15:01:47 -0300

    Fix: Se modifica lógica de deslizamiento con botones de prueba

--- web-codelens/src/components/homeServices/HomeServices.jsx
+++ web-codelens/src/components/homeServices/HomeServices.jsx
@@ -6,29 +6,41 @@ import { useState, useRef } from 'react'
 const HomeServices = ({ t }) => {
   const services = t.cards
   const [activeSlide, setActiveSlide] = useState(0)
-  const [isTouching, setIsTouching] = useState(false)
-  const [prevItemX, setPrevItemX] = useState(undefined)
-  const [prevScrollLeft, setPrevScrollLeft] = useState(undefined)
-  const [diff, setDiff] = useState(undefined)
+  const [isTouching, setIsTouching] = useState(false) // isMousedown
+  const [startX, setStartX] = useState(0)
+  const [scrollLeft, setScrollLeft] = useState(0)
   const carouselRef = useRef(null)
+  const length = services.length
 
   // Guarda valores iniciales de cuando el usuario tocó el carousel
-  const scrollStart = (e) => {
-    console.log(isTouching)
+  const handleMouseDown = (e) => {
+    e.preventDefault()
     setIsTouching(true);
-    setPrevItemX(e.touches[0]?.pageX);
-    setPrevScrollLeft(carouselRef.current.scrollLeft); 
+    setStartX(e.pageX); 
+    console.log("click", isTouching)
     return
   }
 
+  const handleMouseUp = () =>{
+    setIsTouching(false)
+    console.log('Dejó de hacer click', isTouching)
+    return 
+  }
   // Usa los valores de la función ScrollStart para calcular cuánto mover el slider
   const scrolling = (e) => {
-    if(!isTouching) return;
-    let pageX = e.touches[0].pageX;
-    setDiff(pageX - prevItemX);
-    carouselRef.current.scrollLeft = prevScrollLeft - diff;
+    if(!isTouching || !carouselRef.current) return;
+    const deltaX = e.clientX - startX;
+    carouselRef.current.scrollLeft = scrollLeft - deltaX;
     return
   }
+
+  const prevSlide = () => {
+      setActiveSlide(activeSlide === 0 ? length - 1 : activeSlide - 1 )
+  }
+
+  const nextSlide = () => {
+    setActiveSlide(activeSlide === length - 1 ? 0 : activeSlide + 1 )
+}
 
   return (
     <Section className='home-services-section'>
@@ -37,25 +49,37 @@ const HomeServices = ({ t }) => {
         <span>{t.intro}</span>
       </div>
       <div className='home-services-carousel'>
+        <div className="izq" onClick={prevSlide}>
+        {"<--"}
+        </div>
+        <div className="der" onClick={nextSlide}>
+        {"-->"}
+        </div>
         <div 
           className='home-carousel-slides' 
           ref={carouselRef} 
+          style={{ transform: `translateX(${activeSlide * 300}px)` }}
           // Eventos touch
-          onTouchStart={(e) => scrollStart(e)} 
-          onTouchMove={(e) => scrolling(e)}
-          onTouchEnd={() => {
-              setIsTouching(false)
-            }}
+          // onTouchStart={(e) => scrollStart(e)} 
+          // onTouchMove={(e) => scrolling(e)}
+          // onTouchEnd={() => {
+          //     setIsTouching(false)
+          //   }}
+
+          // Eventos mouse
+          // onMouseDown={(e) => {handleMouseDown(e)}}
+          // onMouseMove={(e) => {scrolling(e)}}
+          // onMouseUp={() => {handleMouseUp()}}
         >
           {services.map((service, i) => {
             return <CardHomeService
-              activeClass={activeSlide === i ? "activeSlide" : ""}
               key={i + service.url}
               title={service.title}
               content={service.content}
               icon={service.icon}
               serviceUrl={service.url}
               cta={t['services-card-cta']}
+              
             />
           }
           )}
@@ -74,13 +98,10 @@ const HomeServices = ({ t }) => {
 export default HomeServices;
 
 /*
-- hacer autoScroll
-    -obtener width de la card dinámicamente
-    - hacer globales los valores
-
 
 - Si el usuario scrollea para la izquierda, restar el width de la card y el gap 
 - sino, añadirlo
 
+- Hacer andar botones
 
 */

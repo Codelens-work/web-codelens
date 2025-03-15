86cb54ac0b6a76e8b9df214e6f16036bf69558c8
Loreaylen <lag9512@gmail.com>
2025-03-15 15:23:01 -0300

    Fix: Router
    Layout Routes: Se implementa el componente Outlet en lugar de pasar children

--- src/components/mainLayout/MainLayout.jsx
+++ src/components/mainLayout/MainLayout.jsx
@@ -3,7 +3,7 @@ import Footer from '../footer/Footer';
 import NavBar from '../navBar/NavBar';
 import { Outlet } from 'react-router-dom';
 import './mainLayout.css';
-const MainLayout = ({ children }) => {
+const MainLayout = () => {
     return (
         <div className='main-layout'>
             <NavBar />

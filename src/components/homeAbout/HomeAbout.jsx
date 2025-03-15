6f17b917433981ba550221b701f213d66cce7d1d
Nico <nicolasromero.barrios@gmail.com>
2025-03-15 13:42:51 -0300

    add svg for imgs

--- src/components/homeAbout/HomeAbout.jsx
+++ src/components/homeAbout/HomeAbout.jsx
@@ -6,7 +6,7 @@ const HomeAbout = ({ t, btn, currentLanguage }) => {
   return (
     <Section className="home-about-section">
       <div className="home-about-img">
-        <img src="/imgs/homeIntroImg.webp" alt="About image" />
+        <img src="/imgs/homeIntroImg.svg" alt="About image" />
       </div>
       <div className="home-about-content">
         <img className="home-about-logo" src="/logos/homeAboutLogo.webp" alt="Codelens Logo" />

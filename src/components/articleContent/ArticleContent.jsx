1ea311dba87f851f5bc2b01036d0261d791ac173
Loreaylen <lag9512@gmail.com>
2025-04-06 15:48:31 -0300

    Feat: Integrar blog
    Se instaló html-react-parser para convertir string a html

--- src/components/articleContent/ArticleContent.jsx
+++ src/components/articleContent/ArticleContent.jsx
@@ -1,14 +1,30 @@
+import parse from 'html-react-parser'
+
 const ArticleContent = ({sections}) => {
 
-
+const htmlexample = '<h2>Este es un html de prueba</h2><p>Este html debería mostrarse ok</p>'
   return (
 <>
 {sections.map((section, i) => {
-
-  return <section>{i}</section>
+  const contents = Object.entries(section)
+  return <section key={`section${i}`}>{parse(htmlexample)}</section>
 })}
 </>
   )
 }
 
 export default ArticleContent;
+
+/*
+- Tengo una sección que es un objeto con las tags como keys
+- Por cada sección devuelvo un section, ese section tiene el html con el contenido adentro
+- Tengo que mapear cada objeto para que devuelva <key>value</key>
+- Algunas keys pueden ser arrays, por ejemplo p y ul. Si es ul tiene que mapearse con li adentro.
+- el mapeado anterior se tiene que devolver dentro de cada sección
+<section>
+<key>value</key>
+<key>value</key>
+<key>value</key>
+</section>
+
+*/

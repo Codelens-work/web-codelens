8fcea6456e86b0cb7a35a46a9ee852b702f4c56f
Loreaylen <lag9512@gmail.com>
2025-04-18 23:30:05 -0300

    Feat: integrar blog
    Se elimina componente ArticleContent. Se modifica estructura JSON de artículos. Se incorpora useNavigate para redirigir a 404. Se integra componente SeoMeta

--- src/pages/articlePage/ArticlePage.jsx
+++ src/pages/articlePage/ArticlePage.jsx
@@ -1,10 +1,11 @@
 import { useState, useEffect, useContext } from "react";
-import { useParams, Link, useNavigate } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import { BlogContext } from "../../context/BlogContext";
 import "./articlePage.css";
 import ArticleNavigation from "../../components/articleNavigation/ArticleNavigation";
 import RelatedArticles from "../../components/relatedArticles/RelatedArticles";
+import SeoMeta from "../../components/seoHelmet/SeoMeta";
 import parse from 'html-react-parser'
 
 const ArticlePage = () => {
@@ -13,7 +14,7 @@ const ArticlePage = () => {
   const { i18n } = useTranslation()
   const [article, setArticle] = useState(null);
   const lang = i18n.language
-
+  const navigate = useNavigate()
 
   useEffect(() => {
     const getCurrentArticle = () => {
@@ -25,8 +26,7 @@ const ArticlePage = () => {
         setArticle(currentArticle)
       }
       catch {
-        console.log(error)
-        // Poner lógica para redirigir a 404 indicando que el artículo no existe
+        navigate("/404");
       }
     }
     getCurrentArticle()
@@ -35,14 +35,23 @@ const ArticlePage = () => {
   // Poner otro spinner bonito o el rocket
   if (!article) return <p>Cargando...</p>;
 
-  
+  const canonical = lang === "en"
+  ? `https://codelenstech.com/en/blog/${article.slug.en}`
+  : `https://codelenstech.com/en/blog/${article.slug.es}`
 
   return (
+    <>
+    <SeoMeta
+    title={article.focusKeyword[lang]}
+    description={article.metadescription[lang]}
+    canonical={canonical}
+    keywords={article.focusKeyword[lang]}
+    />
     <div>
       <article className="article-page">
-       <picture className="article-image">
+        <picture className="article-image">
           <img src={article.imgUrl} alt={article.imgAlt[lang]} />
-       </picture>
+        </picture>
         <section className="article-content-container">
           <div className="article-content">
             <div className="content-block">
@@ -56,6 +65,7 @@ const ArticlePage = () => {
       </article>
       <ArticleNavigation currentArticle={slug} />
     </div>
+    </>
   );
 };
 
